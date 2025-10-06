66a58f29265331fe9b3c0d907d314032be05f0ee
Bigwals <bigwalstudio@gmail.com>
2025-10-07 01:27:40 +0500

    pursor optional

--- src/validations/authValidation.ts
+++ src/validations/authValidation.ts
@@ -6,7 +6,7 @@ export const registerSchema = z.object({
   firstName: z.string().min(1, "firstName is Required"),
   lastName: z.string().min(1, "lastName is Required"),
   telephone: z.string().min(1, "telephone is Required"),
-  purser: z.string().min(1, "purser is Required"),
+  purser: z.string().min(1, "purser is Required").optional(),
   speaker: z.string().min(1, "speaker is Required").optional(),
   languages: z.array(z.number()).min(
     1,
