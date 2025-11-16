e9c0328e804d0363e9ef9b1400bb4f5d034217ef
CipherCoreIT <samnorrisiii@gmail.com>
2025-11-16 12:26:54 +0500

    added response time to 5 minute

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,25 @@
+// import app from './app';
+// import dotenv from 'dotenv';
+// import { getPool } from './config/db';
+
+// dotenv.config();
+
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(PORT, async () => {
+//   console.log(`🚀 Server is up & running on port ${PORT}`);
+
+//   try {
+//     const pool = await getPool();
+//     await pool.request().query('SELECT 1');
+//     console.log('✅ Connected to Azure SQL Database');
+//   } catch (err) {
+//     console.error('❌ Database connection failed:', err);
+//   }
+// });
+
+
+import http from 'http';
 import app from './app';
 import dotenv from 'dotenv';
 import { getPool } from './config/db';
@@ -6,7 +28,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, async () => {
+const server = http.createServer(app);
+
+server.timeout = 5 * 60 * 1000; // 5 minutes in milliseconds
+
+server.listen(PORT, async () => {
   console.log(`🚀 Server is up & running on port ${PORT}`);
 
   try {
