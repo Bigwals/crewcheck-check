4f0e88ca6f645425625423b44579c0c8aeb74762
Bigwals <132106450+Bigwals@users.noreply.github.com>
2026-06-01 23:21:13 +0500

    Delete dist directory

--- dist/validations/mediaValidation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mediaSchema = void 0;
-const zod_1 = require("zod");
-exports.mediaSchema = zod_1.z.object({
-    crewId: zod_1.z.string().min(1, "Crew ID is required"),
-    media: zod_1.z.string().min(1, "Media is required"),
-});
